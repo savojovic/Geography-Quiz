61e89fbb393f462688c46f3260801b8df5fd4f73
jovo_savic <jovo.savic00@gmail.com>
2021-11-03 17:57:23 +0100

    Choosing capitals finished

--- app/src/main/java/org/unibl/etf/kviz/CapitalsActivity.java
+++ app/src/main/java/org/unibl/etf/kviz/CapitalsActivity.java
@@ -1,9 +1,13 @@
 package org.unibl.etf.kviz;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -31,6 +35,7 @@ public class CapitalsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_capitals);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onStart() {
         super.onStart();
@@ -42,21 +47,20 @@ public class CapitalsActivity extends AppCompatActivity {
         if(questionNumber<countries.size()&&questionNumber>=0){
             question= getString(R.string.captial_question)+" "+countries.get(questionNumber).countryName+"?";
             textViewQuestion.setText(question);
+            randomiseAnswers();
         }else if(questionNumber<0){
             questionNumber=4;
             askQuestion();
+            randomiseAnswers();
         }else if(questionNumber==CategoriesActivity.NUMBER_OF_QUESTIONS){
             questionNumber=0;
             askQuestion();
+            randomiseAnswers();
         }
-        randomiseAnswers();
-//        btnA.setText(countries.get(questionNumber).city1);
-//        btnB.setText(countries.get(questionNumber).city2);
-//        btnC.setText(countries.get(questionNumber).city3);
-//        btnD.setText(countries.get(questionNumber).capital);
+
     }
     private void randomiseAnswers(){
-        int correctAnswerNum = (int)(Math.random()*NUMEBR_OF_ANSWERS+1);
+        int correctAnswerNum = (int)(Math.random()*NUMEBR_OF_ANSWERS);
         if(correctAnswerNum==0){
             btnA.setText(countries.get(questionNumber).capital);
             btnC.setText(countries.get(questionNumber).city1);
@@ -79,6 +83,16 @@ public class CapitalsActivity extends AppCompatActivity {
             btnD.setText(countries.get(questionNumber).capital);
         }
     }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void setNeutralColor(){
+        TypedValue typedValue = new TypedValue();
+        getTheme().resolveAttribute(R.attr.colorPrimary, typedValue, true);
+        int primaryColor = typedValue.data;
+        btnA.setBackgroundColor(primaryColor);
+        btnB.setBackgroundColor(primaryColor);
+        btnC.setBackgroundColor(primaryColor);
+        btnD.setBackgroundColor(primaryColor);
+    }
     private void getReferences(){
         textViewQuestion = findViewById(R.id.capitals_question);
         btnA=findViewById(R.id.btn_capitals_a);
@@ -88,22 +102,40 @@ public class CapitalsActivity extends AppCompatActivity {
         btnPreviouse=findViewById(R.id.btn_previouse);
         btnNext=findViewById(R.id.btn_next);
     }
+
+    private void setIsClickable(boolean isClickable){
+        btnA.setClickable(isClickable);
+        btnB.setClickable(isClickable);
+        btnC.setClickable(isClickable);
+        btnD.setClickable(isClickable);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
     private void setListeners(){
-//        btnA.setOnClickListener(v->{
-//            String answer = btnA.getText().toString();
-//            if(answer.equals(countries.get(questionNumber).capital)) {
-//                btnA.setBackgroundColor(Color.BLACK);
-//            }else{
-//                btnA.setBackgroundColor(Color.RED);
-//            }
-//        });
+        View.OnClickListener listener = v -> {
+            String answer = ((Button)v).getText().toString();
+            if(answer.equals(countries.get(questionNumber).capital)) {
+                v.setBackgroundColor(Color.GREEN);
+            }else{
+                v.setBackgroundColor(Color.RED);
+            }
+            setIsClickable(false);
+        };
+        btnA.setOnClickListener(listener);
+        btnB.setOnClickListener(listener);
+        btnC.setOnClickListener(listener);
+        btnD.setOnClickListener(listener);
+
         btnNext.setOnClickListener(v->{
+            setIsClickable(true);
             questionNumber++;
             askQuestion();
+            setNeutralColor();
         });
         btnPreviouse.setOnClickListener(v->{
+            setIsClickable(true);
             questionNumber--;
             askQuestion();
+            setNeutralColor();
         });
     }
 }
